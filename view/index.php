4606be2c21e92809afd3d9433dcbe18759193afe
saidioumayma8 <oumaymasaidi908@gmail.com>
2024-01-02 09:27:37 +0100

    bin

--- view/index.php
+++ view/index.php
@@ -2,135 +2,160 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="./assets/css/style.css" rel="stylesheet" >
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="../public/css/style.css" rel="stylesheet">
+  <title>Document</title>
 </head>
 
 <body>
 
 
-<nav class="navbar navbar_expand_lg fixed-top">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand me_auto" href="#">logo</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#">SmatrTravel</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx_lg_2" href="#">about</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx_lg_2" href="#">about</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx_lg_2" href="#">about</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx_lg_2" href="#">about</a>
-          </li>
-        </ul>
-      </div>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
     </div>
-    <a href="#" class="login-button">Login</a>
-    <button class="nvbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation bar">
-        <span class="navbar-toggler-icon"></span>
-    </button>
   </div>
 </nav>
-<div class="section_top">
-    <div class="content">
-        <h1>Sagar <span>Developer</span></h1>
-        <a href="#">Welcome</a>
-    </div>
-</div>
-<div class="form-body">
-        <div class="row">
-            <div class="form-holder">
-                <div class="form-content">
-                    <div class="form-items">
-                        <h3>Register Today</h3>
-                        <p>Fill in the data below.</p>
-                        <form class="requires-validation" novalidate>
+  <div class="">
+    <div class="section_top row d-flex align-items-center ">
+      <div class="text-section col-6 px-5">
+        <h1 class="primary-title">smart<span>travele</span></h1>
+        <p class="welcome">Welcome aboard! Explore the Morocco with ease as you embark on a journey of comfort and adventure. Our buses are your ticket to unforgettable destinations. Fasten your seatbelt and let the travel experience begin!</p>
+      </div>
+      <div class="content col-6">
+        <div class="form-holder">
+          <div class="form-content">
+            <div>
+              <h3>Recherche un voyage</h3>
 
-                            <div class="col-md-12">
-                               <input class="form-control" type="text" name="name" placeholder="Full Name" required>
-                               <div class="valid-feedback">Username field is valid!</div>
-                               <div class="invalid-feedback">Username field cannot be blank!</div>
-                            </div>
+              <form>
 
-                            <div class="col-md-12">
-                                <input class="form-control" type="email" name="email" placeholder="E-mail Address" required>
-                                 <div class="valid-feedback">Email field is valid!</div>
-                                 <div class="invalid-feedback">Email field cannot be blank!</div>
-                            </div>
-
-                           <div class="col-md-12">
-                                <select class="form-select mt-3" required>
-                                      <option selected disabled value="">Position</option>
-                                      <option value="jweb">Junior Web Developer</option>
-                                      <option value="sweb">Senior Web Developer</option>
-                                      <option value="pmanager">Project Manager</option>
-                               </select>
-                                <div class="valid-feedback">You selected a position!</div>
-                                <div class="invalid-feedback">Please select a position!</div>
-                           </div>
-
-
-                           <div class="col-md-12">
-                              <input class="form-control" type="password" name="password" placeholder="Password" required>
-                               <div class="valid-feedback">Password field is valid!</div>
-                               <div class="invalid-feedback">Password field cannot be blank!</div>
-                           </div>
-
-
-                           <div class="col-md-12 mt-3">
-                            <label class="mb-3 mr-1" for="gender">Gender: </label>
-
-                            <input type="radio" class="btn-check" name="gender" id="male" autocomplete="off" required>
-                            <label class="btn btn-sm btn-outline-secondary" for="male">Male</label>
-
-                            <input type="radio" class="btn-check" name="gender" id="female" autocomplete="off" required>
-                            <label class="btn btn-sm btn-outline-secondary" for="female">Female</label>
-
-                            <input type="radio" class="btn-check" name="gender" id="secret" autocomplete="off" required>
-                            <label class="btn btn-sm btn-outline-secondary" for="secret">Secret</label>
-                               <div class="valid-feedback mv-up">You selected a gender!</div>
-                                <div class="invalid-feedback mv-up">Please select a gender!</div>
-                            </div>
-
-                        <div class="form-check">
-                          <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-                          <label class="form-check-label">I confirm that all data are correct</label>
-                         <div class="invalid-feedback">Please confirm that the entered data are all correct!</div>
-                        </div>
-                  
-
-                            <div class="form-button mt-3">
-                                <button id="submit" type="submit" class="btn btn-primary">Register</button>
-                            </div>
-                        </form>
-                    </div>
+                <div class="col-md-12">
+                  <input class="form-control" type="text" name="Depart" placeholder="Depart" required>
                 </div>
+
+                <div class="col-md-12">
+                  <input class="form-control" type="text" name="Areeve" placeholder="Areeve" required>
+                </div>
+
+                <div class="col-md-12">
+                  <input class="form-control" type="date" name="datetime" placeholder="date" required>
+                </div>
+
+                <div class="col-md-12">
+                  <input class="form-control" type="numbere" name="datetime" placeholder="voayageures" required>
+                </div>
+
+                <div class="form-button mt-3">
+                  <button id="submit" type="submit" class="btn btn-primary">Recherch</button>
+                </div>
+              </form>
             </div>
+          </div>
         </div>
+      </div>
     </div>
 
 
+  </div>
+  </div>
+ 
+ 
+      <div class="row" class="welcome">
+  <div class="col-md-4">
+    <img src="../public/image/imgs (1)/imgs/ctm.jpg" alt="">
+    <h3>Ctm</h3>
+  </div>
+  
+  <div class="col-md-4">
+    <img src="../public/image/imgs (1)/imgs/JanaViajes.jpg" alt="">
+    <h3>JanaViajes</h3>
+  </div>
+  
+  <div class="col-md-4">
+    <img src="../public/image/imgs (1)/imgs/BabAllah.jpg" alt="">
+    <h3>BabAllah</h3>
+  </div>
 </div>
-<script></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+      <!-- <div class="slide-container">
+        <span class="slider-span" id="slider-span1"></span>
+        <span class="slider-span" id="slider-span2"></span>
+        <span class="slider-span" id="slider-span3"></span>
+        <span class="slider-span" id="slider-span4"></span>
+        <span class="slider-span" id="slider-span5"></span>
+        <span class="slider-span" id="slider-span6"></span>
+        <span class="slider-span" id="slider-span7"></span>
+
+        <div class="image-slider">
+        <div class="slides-div" id="slider-1">
+          <img src="../public/image/839444-2017-Tourismo-M-3-RHD-Mercedes-Benz-Bus.jpg" alt="" class="img" id="img1">
+          <a href="#slider-span1" class="button" id="button-1"></a>
+         
+
+        </div>
+        <div class="slides-div" id="slider-2">
+          <img src="../public/image/839444-2017-Tourismo-M-3-RHD-Mercedes-Benz-Bus.jpg" alt="" class="img" id="img2">
+          <a href="#slider-span2" class="button" id="button-2"></a>
+         
+
+        </div>
+        <div class="slides-div" id="slider-3">
+          <img src="../public/image/839444-2017-Tourismo-M-3-RHD-Mercedes-Benz-Bus.jpg" alt="" class="img" id="img3">
+          <a href="#slider-span3" class="button" id="button-3"></a>
+         
+
+        </div>
+        <div class="slides-div" id="slider-4">
+          <img src="../public/image/839444-2017-Tourismo-M-3-RHD-Mercedes-Benz-Bus.jpg" alt="" class="img" id="img4">
+          <a href="#slider-span1" class="button" id="button-4"></a>
+         
+
+        </div>
+        <div class="slides-div" id="slider-5">
+          <img src="../public/image/839444-2017-Tourismo-M-3-RHD-Mercedes-Benz-Bus.jpg" alt="" class="img" id="img5">
+          <a href="#slider-span5" class="button" id="button-5"></a>
+         
+
+        </div>
+        <div class="slides-div" id="slider-6">
+          <img src="../public/image/839444-2017-Tourismo-M-3-RHD-Mercedes-Benz-Bus.jpg" alt="" class="img" id="img6">
+          <a href="#slider-span6" class="button" id="button-6"></a>
+         
+
+        </div>
+        <div class="slides-div" id="slider-7">
+          <img src="../public/image/876285-Tourismo-Mercedes-Benz-Bus-Evening-Silver-color.jpg" alt="" class="img" id="img7">
+          <a href="#slider-span7" class="button" id="button-7"></a>
+         
+
+        </div> 
+
+      </div>-->
+  
+
+  
+  <script src="../public/js/app.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ voayege.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link href="../public/css/style.css" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">SmatrTravel</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./view/index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="card" style="width: 60rem;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="./public/image/imgs (1)/imgs/BabAllah.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-4">
+      <div class="card-body">
+        <p class="card-text">
+          <i class="fas fa-map-marker-alt"></i> Depart: 20:30
+          <i class="fas fa-arrow-right"></i>
+          <i class="fas fa-map-marker-alt"></i> Areeve: 23:00
+        </p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h3>100 Dh</h3>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</div>
+
+
+</div>
+</body>
+</html>
